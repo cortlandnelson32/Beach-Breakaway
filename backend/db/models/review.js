0cf16384d6beef5b4b69a2da6844939d5b7b9701
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-08-01 19:57:20 -0400

    done spot and review migration/model

---
+++ backend/db/models/review.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Review.belongsTo(models.spot, {
+        foreignKey: spotId
+      })
+      Review.belongsTo(models.user, {
+        foreignKey: userId
+      })
+    }
+  }
+  Review.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
