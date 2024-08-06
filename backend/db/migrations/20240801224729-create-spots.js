bcc1820450df744a828b41c74b11851110672265
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-08-06 12:08:11 -0400

    done associations

--- backend/db/migrations/20240801224729-create-spots.js
+++ backend/db/migrations/20240801224729-create-spots.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -67,9 +73,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = "Users";
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Spot.hasMany(models.Review,{
+        foreignKey: 'spotId'
+      })
     }
   }
   Spot.init({
