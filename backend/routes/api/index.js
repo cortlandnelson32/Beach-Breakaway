cff6fdeff6eaaae5c0eb54a11a6bdcd383e5feac
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-11-22 08:18:36 -0500

    minor change

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,3 @@
-
-// backend/routes/api/index.js
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
@@ -10,9 +8,6 @@ const reviewImagesRouter = require('./review-images.js');
 const bookingRouter = require('./bookings.js');
 const { restoreUser } = require("../../utils/auth.js");
 
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
